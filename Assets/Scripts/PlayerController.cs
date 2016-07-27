648bccefd365d94974f4a17a017975d3f3619a5b
cc-setwo <cc.setwo@gmail.com>
2016-07-27 17:58:10 +0300

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public float MooveSpeed = 0.1f;
+    private Rigidbody rb;
+    private float count;
+
+    public Text CountText;
+    public Text WinText;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        WinText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 m=new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(m* MooveSpeed);
+        
+    }
+    void OnTriggerEnter(Collider cl)
+    {
+        if (cl.gameObject.CompareTag("Coin"))
+        {
+             cl.gameObject.SetActive(false);
+            count+=0.5f;
+            SetCountText();
+        }
+            
+    }
+    void SetCountText()
+    {
+        CountText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            WinText.text = "You win!";
+        }
+    }
+}
